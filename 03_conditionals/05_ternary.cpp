4e214751dafd11216e029e83799cf7c36353f70c
Raj <rajsecret14@gmail.com>
2022-10-21 01:11:09 +0530

    d

---
+++ 03_conditionals/05_ternary.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+// ternary is substitute of if else
+    int marks;
+    cin>>marks;
+    marks>33 ? cout<<"pass" : cout<<"fail";
+    
+
+
+
+    return 0;
+}
