0a5386138e252804d8e40bc2fcbc13c532063276
Raj <rajsecret14@gmail.com>
2022-11-19 00:14:02 +0530

    19nov2022_12:14

---
+++ 06_Functions/03_standard_library_functions.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+
+    cout<<pow(2,30)<<endl;
+    cout<<sqrt(17)<<endl; // output and input is double
+
+    int ans = sqrt(17);
+    cout<<ans<<endl;
+
+    double doubleans = sqrt(17);
+    cout<<doubleans<<endl;
+
+
+
+
+
+    
+
+
+
+
+    return 0;
+}
---
+++ 06_Functions/04_mcq.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+    void sum(int x , int y){
+        cout<<(x+y);
+    }
+
+    int main (){
+        int p = 10;
+        sum(p,32);
+    
+
+    
+
+
+
+
+    return 0;
+}
